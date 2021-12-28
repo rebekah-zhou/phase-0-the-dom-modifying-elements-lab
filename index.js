3992d14f4b997c1ff7a05bf4f6feda50ce422cb7
Rebekah Zhou <rebekahzhou@gmail.com>
2021-12-28 14:14:57 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removeThis = document.getElementsByTagName("main")[0];
+removeThis.remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = ("Rebekah is the champion!");
+newHeader.id = ("victory");
