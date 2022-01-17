76af87c3e593f3f43b7047f51cf8e06f28e88d03
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-17 15:19:37 -0800

    commit tree d250eaa8edd7b69a1aa30f1ad19777b19715aa65

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Write your code here!
-const removeThis = document.getElementsByTagName("main")[0];
-removeThis.remove();
-const newHeader = document.createElement("h1");
-newHeader.textContent = ("Rebekah is the champion!");
-newHeader.id = ("victory");
+const element = document.getElementById("main")
+element.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.textContent = "Rebekah is the champion"
+newHeader.id = "victory"
+document.body.append(newHeader)
+
